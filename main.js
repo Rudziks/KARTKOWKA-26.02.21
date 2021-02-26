545529f6a6f0a65fc4d73e17290ff57254227ccc
Roza Rudziks <Rozvilla12@o2.pl>
2021-02-26 13:51:23 +0100

    skonczone

---
+++ main.js
@@ -0,0 +1,34 @@
+const arr = new Array(1, 15, 150, 1500, "Jan", 5525, "Henryk");
+console.log(arr)
+//zadanie 1
+arr.forEach(elementy =>{
+    console.log(elementy)
+})
+
+//zadanie 2 
+const wszytsko = arr.every(item =>{
+    if(item >=10){
+        return item
+    }
+})
+console.log(wszytsko)
+
+//zadanie 3
+const index = arr.findIndex(imie => imie === "Jan")
+console.log(index)
+console.log(arr[index])
+
+//zadanie na 6
+const liczby = new Array(1, 15, 150, 1500, 5525)
+const imiona = new Array("Jan", "Henryk")
+
+const imionaLiczby = (liczby + ' ' + imiona)
+console.log(imionaLiczby)
+
+
+
+
+
+
+
+
